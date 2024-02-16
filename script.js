ad17a8c0d55e0fe934ab972a620eba70d6f4d612
Md. Sazedur Rahman <sazedur15-2169@diu.edu.bd>
2024-02-17 02:56:21 +0600

    contact form added

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-var writeMessageBtn = document.getElementById('write_message_btn');
-        var contactForm = document.getElementById('contact_form');
+    var writeMessageBtn = document.getElementById('write_message_btn');
+    var contactForm = document.getElementById('contact_form');
+    var submitBtn = document.getElementById('submit_btn');
 
         writeMessageBtn.addEventListener('click', function () {
             if (contactForm.style.display === 'none' || contactForm.style.display === '') {
@@ -9,4 +10,9 @@ var writeMessageBtn = document.getElementById('write_message_btn');
                 contactForm.style.display = 'none';
                 writeMessageBtn.textContent = 'Write Message';
             }
+            
+        });
+        submitBtn.addEventListener('click', function (event) {
+            event.preventDefault(); // Prevents the form from submitting and reloading the page
+            alert('Message Sent'); // Display an alert message
         });
